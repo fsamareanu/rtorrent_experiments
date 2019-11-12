34dd6dc97e9cd33fdf56636ae5d806c959f94373
Florin Samareanu <florin.samareanu@gmail.com>
2019-11-12 13:30:20 +0100

    Sync

--- rtorrent.sh
+++ rtorrent.sh
@@ -6,7 +6,7 @@ LIBTORRENT_VER="0.13.8"
 RTORRENT_VER="0.9.8"
 #XMLRPC_VER="1.39.12"
 XMLRPC_VER="1.39.13"
-PKG_RELEASE="3"
+PKG_RELEASE="1"
 PREFIX="/usr"
 CFLAGS="-march=native -pipe -O2 -fomit-frame-pointer${CFLAGS:+ }${CFLAGS}"
 export CFLAGS
@@ -16,14 +16,23 @@ LIBTORRENT_URL="http://rtorrent.net/downloads/libtorrent-$LIBTORRENT_VER.tar.gz"
 RTORRENT_URL="http://rtorrent.net/downloads/rtorrent-$RTORRENT_VER.tar.gz"
 XMLRPC_URL="https://sourceforge.net/projects/xmlrpc-c/files/Xmlrpc-c%20Super%20Stable/$XMLRPC_VER/xmlrpc-c-$XMLRPC_VER.tgz/download"
 
-# Misc.
-SRC_DIR="$HOME/src/build/rtorrent_experiments/sources"
-DEB_DIR="$HOME/src/build/rtorrent_experiments/binaries"
-PATCH_DIR="$HOME/src/build/rtorrent_experiments/vanilla-patches"
-PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
-export PKG_CONFIG_PATH
+#Misc
+CHECKINSTALL_OPTS="--maintainer=foobar@Build.chmuranet.net"
 
 # ==> MAIN PROGRAM <==
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        instdir="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$instdir/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+#Get the main directory where script resides
+INSTDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+SRC_DIR="$INSTDIR/sources"
+DEB_DIR="$INSTDIR/binaries"
+PATCH_DIR="$INSTDIR/vanilla-patches"
+
 set -e
 
 # Remove old versions
@@ -54,7 +63,7 @@ fi
 cd $SRC_DIR/xmlrpc-c-$XMLRPC_VER
 ./configure --prefix="$PREFIX" --enable-libxml2-backend --disable-libwww-client --disable-wininet-client --disable-abyss-server --disable-cgi-server --disable-cplusplus
 make -j$(nproc)
-sudo checkinstall -D --pkgrelease="$PKG_RELEASE" --install=no -y
+sudo checkinstall -D "$CHECKINSTALL_OPTS" --pkgrelease="$PKG_RELEASE" --install=no -y
 sleep 3
 cp $SRC_DIR/xmlrpc-c-$XMLRPC_VER/xmlrpc-c_"$XMLRPC_VER"-"$PKG_RELEASE"_amd64.deb $DEB_DIR
 sudo rm -rf $SRC_DIR/xmlrpc-c-$XMLRPC_VER
@@ -77,7 +86,7 @@ cd $SRC_DIR/libtorrent-$LIBTORRENT_VER
 ./autogen.sh
 ./configure --prefix="$PREFIX" --disable-debug --with-posix-fallocate
 make -j$(nproc)
-sudo checkinstall -D --pkgrelease="$PKG_RELEASE" --install=no -y
+sudo checkinstall -D "$CHECKINSTALL_OPTS" --pkgrelease="$PKG_RELEASE" --install=no -y
 sleep 3
 cp $SRC_DIR/libtorrent-$LIBTORRENT_VER/libtorrent_"$LIBTORRENT_VER"-"$PKG_RELEASE"_amd64.deb $DEB_DIR
 sudo rm -rf $SRC_DIR/libtorrent-$LIBTORRENT_VER
@@ -97,7 +106,7 @@ cd $SRC_DIR/rtorrent-$RTORRENT_VER
 ./autogen.sh
 ./configure --prefix="$PREFIX" --with-xmlrpc-c
 make -j$(nproc)
-sudo checkinstall -D --pkgrelease="$PKG_RELEASE" --install=no -y
+sudo checkinstall -D "$CHECKINSTALL_OPTS" --pkgrelease="$PKG_RELEASE" --install=no -y
 sleep 3
 cp $SRC_DIR/rtorrent-$RTORRENT_VER/rtorrent_"$RTORRENT_VER"-"$PKG_RELEASE"_amd64.deb $DEB_DIR
 sudo rm -rf $SRC_DIR/rtorrent-$RTORRENT_VER
